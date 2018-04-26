c97d368a3ab52f6ee375fcc439ef0e59415e381f
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-04-26 22:39:25 +0100

    Update IrisData.py
    Defined the different parts of the flower into subsets of rows to get max min and mean of each part of each flower

--- IrisData.py
+++ IrisData.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as pl
+import pandas
 import math
 
 with open("data/iris.csv") as data:
@@ -10,37 +11,79 @@ with open("data/iris.csv") as data:
     data = np.genfromtxt("data/iris.csv", delimiter = ',')
 
 # Name the columns of data below
-petallength = data[:,0]
-petalwidth = data[:,1]
-sepallength = data[:,2]
-sepalwidth = data[:,3]
 
+setosasepallength = data[:50,0]
+setosasepalwidth = data[:50,1]
+setosapetallength = data[:50,2]
+setosapetalwidth = data[:50,3]
 
-# calculate the mean of each column
-meanpetallength = np.mean(data[:,0])
-meanpetalwidth = np.mean(data[:,1])
-meansepallength = np.mean(data[:,2])
-meansepalwidth = np.mean(data[:,3])
+versicolorsepallength = data[50:50,0]
+versicolorsepalwidth = data[50:50,1]
+versicolorpetallength = data[50:50,2]
+versicolorpetalwidth = data[50:50,3]
+
+virginicasepallength = data[100:50,0]
+virginicasepalwidth = data[100:50,1]
+virginicapetallength = data[100:50,2]
+virginicapetalwidth = data[100:50,3]
+
+# calculate the mean of each measurement for each of the 3 species
+meansetosasepallength = np.mean(setosasepallength)
+meansetosasepalwidth = np.mean(setosasepalwidth)
+meansetosapetallength = np.mean(setosapetallength)
+meansetosapetalwidth = np.mean(setosapetalwidth)
+
+meanversicolorsepallength = np.mean(versicolorsepallength)
+meanversicolorsepalwidth = np.mean(versicolorsepalwidth)
+meanversicolorpetallength = np.mean(versicolorpetallength)
+meanversicolorpetalwidth = np.mean(versicolorpetalwidth)
+
+meanvirginicasepallength = np.mean(virginicasepallength)
+meanvirginicasepalwidth = np.mean(virginicasepalwidth)
+meanvirginicapetallength = np.mean(virginicapetallength)
+meanvirginicapetalwidth = np.mean(virginicapetalwidth)
+
 
 # calculate the max of each column
-maxpetallength = np.max(data[:,0])
-maxpetalwidth = np.max(data[:,1])
-maxsepallength = np.max(data[:,2])
-maxsepalwidth = np.max(data[:,3])
+maxsetosasepallength = np.max(setosasepallength)
+maxsetosasepalwidth = np.max(setosasepalwidth)
+maxsetosapetallength = np.max(setosapetallength)
+maxsetosapetalwidth = np.max(setosapetalwidth)
+
+maxversicolorsepallength = np.max(versicolorsepallength)
+maxversicolorsepalwidth = np.max(versicolorsepalwidth)
+maxversicolorpetallength = np.max(versicolorpetallength)
+maxversicolorpetalwidth = np.max(versicolorpetalwidth)
+
+maxvirginicasepallength = np.max(virginicasepallength)
+maxvirginicasepalwidth = np.max(virginicasepalwidth)
+maxvirginicapetallength = np.max(virginicapetallength)
+maxvirginicapetalwidth = np.max(virginicapetalwidth)
 
 # calcualte the min of each column
-minpetallength = np.min(data[:,0])
-minpetalwidth = np.min(data[:,1])
-minsepallength = np.min(data[:,2])
-minsepalwidth = np.min(data[:,3])
+minsetosasepallength = np.min(setosasepallength)
+minsetosasepalwidth = np.min(setosasepalwidth)
+minsetosapetallength = np.min(setosapetallength)
+minsetosapetalwidth = np.min(setosapetalwidth)
 
-pl.hist(petallength)
+minversicolorsepallength = np.min(versicolorsepallength)
+minversicolorsepalwidth = np.min(versicolorsepalwidth)
+minversicolorpetallength = np.min(versicolorpetallength)
+minversicolorpetalwidth = np.min(versicolorpetalwidth)
+
+minvirginicasepallength = np.min(virginicasepallength)
+minvirginicasepalwidth = np.min(virginicasepalwidth)
+minvirginicapetallength = np.min(virginicapetallength)
+minvirginicapetalwidth = np.min(virginicapetalwidth)
+
+
+pl.scatter(setosapetallength)
 pl.show()
-pl.plot(petallength)
+pl.plot(setosapetallength)
 pl.show()
-pl.hist(petalwidth)
+pl.hist(setosapetalwidth)
 pl.show()
-pl.plot(petalwidth)
+pl.plot(setosapetalwidth)
 pl.show()
 
 # http://scikit-learn.org/stable/auto_examples/datasets/plot_iris_dataset.html
@@ -50,10 +93,18 @@ pl.ylabel('Sepal width')
 
 
 # print the mean of each column from above
-print("The average petal length is:", meanpetallength) 
-print("The average petal width is:", meanpetalwidth) 
-print("The average sepal length is:", meansepallength) 
-print("The average sepal width is:", meansepalwidth) 
+print("The average setosa sepal length is:", meansetosasepallength) 
+print("The average virginica sepal length is:", meanvirginicasepallength)
+print("The average versicolor sepal length is:", meanversicolorsepallength)
+
+print("The average setosa sepal length is:", meansetosasepallength) 
+print("The average virginica sepal length is:", meanvirginicasepallength)
+print("The average versicolor sepal length is:", meanversicolorsepallength)  
+
+print("The average setosa petal length is:", meansetosapetallength) 
+print("The average setosa petal width is:", meansetosapetalwidth) 
+print("The average setosa sepal length is:", meansetosasepallength) 
+print("The average setosa sepal width is:", meansetosasepalwidth) 
 
 # print the max of each column from above
 print("The max petal length is:", maxpetallength) 
