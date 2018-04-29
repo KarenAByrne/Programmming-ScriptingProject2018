9561d89929e2ef88d02ebb24c0b7b5e6985316f6
KarenAByrne <36339636+KarenAByrne@users.noreply.github.com>
2018-04-29 12:08:34 +0100

    Update Seaborn.py
    Short description, could not run code

--- Seaborn.py
+++ Seaborn.py
@@ -1,3 +1,9 @@
+# Karen Byrne 29th April 2018
+#https://stackoverflow.com/questions/45862223/use-different-colors-in-scatterplot-for-iris-dataset?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+# I found this code which returns a scatter plot showing the clusters in the data
+#I could not get the file to run correctly in time for the deadline but it is somwthing to work from
+
+
 import matplotlib.pyplot as plt
 import seaborn as sns
 iris = sns.load_dataset("data/iristest.csv")
